6ae9d8091a4591726818d799cc5a6971c74192a0
Shashwat Aneja <shashwataneja1401@gmail.com>
2025-11-20 22:20:00 +0530

    Update healthz.ts

--- api/healthz.ts
+++ api/healthz.ts
@@ -1,5 +1,3 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
-
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  res.status(200).json({ status: 'healthy' });
+export default function handler(req, res) {
+  res.status(200).json({ status: "healthy" });
 }
